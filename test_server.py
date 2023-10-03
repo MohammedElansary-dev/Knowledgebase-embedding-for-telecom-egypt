249db7a3574a3dadc3d6b205d709d11c7c04122e
ZanatyMahmoud <146882108+ZanatyMahmoud@users.noreply.github.com>
2023-10-03 22:20:49 +0300

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -4,7 +4,7 @@ print(
     requests.post(
         "http://0.0.0.0:10000",
         json={
-            "message": "Would be very interesting in having a conversation. Where are you based?"
+            "message": "Start typing here"
         }
     ).json()
 )
