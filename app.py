8ec06be80db6bd6bae1c3dc6be12cd3ec4109a44
MohammedElansary <122054378+MohammedElansary-dev@users.noreply.github.com>
2023-10-04 20:01:13 +0300

    added a way to add charts to the text made by ai
    still working on it

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import guidance
+import urllib.parse
+
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -6,6 +9,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -58,7 +62,9 @@ chain = LLMChain(llm=llm, prompt=prompt)
 def generate_response(message):
     ai_answer = retrieve_info(message)
     response = chain.run(message=message, ai_answer=ai_answer)
-    return response
+    chart_output = generate_chart(message)  # Generate chart based on user message
+    return response, chart_output
+
 
 
 
@@ -68,15 +74,58 @@ def main():
         page_title="Assistant financial analyst for Telecom Egypt", page_icon=":book:")
 
     st.header("Assistant financial analyst for Telecom Egypt :book:")
-    message = st.text_area("by Mohammed Elansary")
+    message = st.text_area("user quetion")
 
     if message:
         st.write("Generating you answer...")
+        
+        response, chart_output = generate_response(message)
 
-        result = generate_response(message)
-
-        st.info(result)
+        st.info(response)
+        st.image(chart_output)  # Display the generated chart as an image
 
 
 if __name__ == '__main__':
     main()
+
+# 6. Function for generating charts using guidance library
+def generate_chart(query):
+    def parse_chart_link(chart_details):
+        encoded_chart_details = urllib.parse.quote(chart_details, safe='')
+        output = "![](https://quickchart.io/chart?c=" + encoded_chart_details + ")"
+        return output
+    
+    examples = [
+        {
+            'input': "Make a chart of the 5 tallest mountains",
+            'output': {"type":"bar","data":{"labels":["Mount Everest","K2","Kangchenjunga","Lhotse","Makalu"], "datasets":[{"label":"Height (m)","data":[8848,8611,8586,8516,8485]}]}}
+        },
+        {
+            'input': "Create a pie chart showing the population of the world by continent",
+            'output': {"type":"pie","data":{"labels":["Africa","Asia","Europe","North America","South America","Oceania"], "datasets":[{"label":"Population (millions)","data": [1235.5,4436.6,738.8,571.4,422.5,41.3]}]}}
+        }
+    ]
+
+    gpt3 = guidance.llms.OpenAI("gpt-3.5-turbo-16k-0613")
+
+
+    chart_program = guidance(
+        '''
+        {{#block hidden=True~}}
+        You are a world class financial analysist. , You will generate chart output based on natural language;
+
+        {{~#each examples}}
+        Q:{{this.input}}
+        A:{{this.output}}
+        ---
+        {{~/each}}
+        Q:{{query}}
+        A:{{gen 'chart' temperature=0 max_tokens=500}}
+        {{/block~}}
+        
+        Hello, here is the chart you requested:
+        {{parse_chart_link chart}}
+        ''')
+
+    chart_output = chart_program(query=query, examples=examples, parse_chart_link=parse_chart_link)
+    return chart_output
