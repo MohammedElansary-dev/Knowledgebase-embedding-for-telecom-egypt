f65bf0a61fda5038987491ba520324aa0a1a106b
MohammedElansary <122054378+MohammedElansary-dev@users.noreply.github.com>
2023-10-04 21:04:44 +0300

    edit to add a chart to the response

--- app.py
+++ app.py
@@ -33,12 +33,19 @@ def retrieve_info(query):
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
 
 template = """
-You are a world class financial analysist. 
+now You are a world class financial analysist. 
 I will give you questions about the company (telecom Egypt we) and you will give me the most comprehensive answer. 
 and you will follow ALL of the rules below:
 1/ Response should be informative and try to say what the numbers mean compare to other years.
 2/ the user is not a financial expert so try to explain as much as you can
 3/ numbers is not in dollars it is in Egyptian pound
+4/ if the user asked you to make a chart for somthing do it in this way: 
+first if he asked you to make a bar chart of the Q1, Q2, Q3, and Q4 for the year 2022 and he want you to plot the expenses and revenue and the expenses for each quarter is 50,60,70,180 and for the revenue is 100,200,300,400, you need to convert the data that he gave you to make it like that {type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:' expenses ',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}
+second add this url to the start of it https://quickchart.io/chart?c= so that it dose look like that in the end https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:' expenses ',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}
+third add it in the best place in your explanation
+fourth it is important to note don't add any thing before or after the chart link just add it as is don't add brackets around it or don't tell me that this is a chart link like this [Chart Link] just but the link  
+know that it is preferred to make a chart 
+
 Below is what the user asked:
 {message}
 
