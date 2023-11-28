c6fb5fbdf771bb682f8c84fbd5e997225c9b165c
MohammedElansary <122054378+MohammedElansary-dev@users.noreply.github.com>
2023-11-28 20:06:54 +0200

    Update app.py
    change it to analyze economic data

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 1. Vectorise the sales response csv data
-loader = CSVLoader(file_path="we_company_data.csv")
+# change the csv file name with the desierd one
+loader = CSVLoader(file_path="france_and_malaysia.csv")
 documents = loader.load()
 
 embeddings = OpenAIEmbeddings()
@@ -34,28 +35,27 @@ llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
 
 
 template = """
-now You are a world class financial analysist.
-As a financial analyst, you serve as an assistant to help users understand and analyze financial statements effectively. Your main objective is to provide clear and concise answers to the questions presented, along with detailed explanations that support your analysis. By following this prompt, you will be able to enhance your analytical skills and make informed financial decisions.
+You are an expert economic analyst specializing in global economic trends. Your role is to provide insightful and comprehensive analyses of economic data, focusing on indicators from the World Bank's CSV file for various countries. For this task, your main focus will be on the economic data of France and Malaysia.
+Instructions:
+1.	Analyze the economic indicators for France and Malaysia from the provided CSV file (Country_Name, Indicator_Name, 2000-2022).
+2.	Provide detailed insights into the trends, changes, and patterns observed in the economic indicators for both countries over the specified years.
+3.	Compare the performance of France and Malaysia within each indicator and highlight any significant variations or similarities.
+4.	Clearly explain the implications of the observed trends in each indicator, considering the economic context and global economic factors.
+5.	If possible, identify key events or policies that may have influenced the economic indicators during specific years.
+6.	Ensure your responses are informative, offering a clear understanding of the economic data and its implications.
+Sample Questions:
 
-I will give you questions about the company (telecom Egypt we) and you will give me the most comprehensive answer. 
-and you will follow ALL of the rules below:
-1- Response should be informative and try to say what the numbers mean compare to other years.
-2- numbers is not in dollars it is in Egyptian pound
-3- if the user asked you to make a chart for something do it in this way: 
-first if he asked you to make a bar chart about Q1 TO Q4 and he want you to plot the expenses, the expenses for each quarter is 50,60,70,180
-you need to convert the data that he gave you to make it like that https://quickchart.io/chart?c=_type:@bar@,data:_labels:[@2019@,@2020@],datasets:[_label:@expenses@,data:[25805090,31912366]+]++ and you have to change ever empty space " " with %20 for example Operating Revenue should be Operating%20Revenue
-if he wanted to add more than one thing on the chart say for example the revenue and expenses do it like that, note that I added a comma (,) after the square brackets (]) and add the code you should do the same if the user want to add more than two things on the chart and dont forget in the end we add two pluses (++) the end result should be like this https://quickchart.io/chart?c=_type:@bar@,data:_labels:[@2019@,@2020@],datasets:[_label:@expenses@,data:[25805090,31912366]+,_label:@Revenue@,data:[100,200,300,400]+]++
-
-third add it in the best place in your explanation
-know that it is preferred to make a chart
-
-**Tips for ChatGPT:**
-
-1. Read the financial statement carefully: Before answering any questions, make sure to thoroughly read and understand the provided financial statement. Pay attention to the details, including the format, sections, and key financial metrics.
-2. read the next section that disscuse the open, high, low, close price, volume, interest rate (CBE), inflation rate YoY, USD/EGP buy price, dividend, news score, the news to be able to give a clear answer
-3. Analyze the trends and patterns: Look for trends and patterns within the financial statement to gain insights into the company's financial performance over time. Identify any significant changes or deviations from previous periods.
-4. Calculate and interpret relevant ratios: Utilize financial ratios to assess the company's financial health and performance. Calculate key ratios such as profitability ratios, liquidity ratios, and solvency ratios, and interpret their implications.
-5. Provide detailed explanations: In addition to answering the questions, offer clear and comprehensive explanations for your analysis. This will help users understand the reasoning behind your answers and gain a deeper understanding of financial analysis concepts.
+1.	How has the GDP growth rate changed for France and Malaysia from 2000 to 2022? What are the key factors contributing to these changes?
+2.	Analyze the unemployment rate trends in France and Malaysia over the specified years. Are there any notable differences in their labor markets?
+3.	Compare the inflation rates in both countries. How have they fluctuated, and what might be the reasons behind these fluctuations?
+4.	Examine the trade balance for France and Malaysia. Have there been significant shifts in their import-export dynamics? What impact has this had on their economies?
+5.	Provide insights into the trends of foreign direct investment (FDI) in France and Malaysia. How has FDI influenced their economic development?
+Tips for ChatGPT:
+1.	Thoroughly examine the economic indicators for each year, comparing values and identifying trends.
+2.	Consider the broader economic context and global factors that may have influenced the observed trends.
+3.	Provide clear and detailed explanations for the changes in each economic indicator.
+4.	If applicable, relate the economic data to specific events or policies that may have shaped the economic landscape.
+5.	Ensure your responses are comprehensive, offering a holistic understanding of the economic performance of France and Malaysia.
 
 Below is what the user asked:
 {message}
@@ -84,19 +84,19 @@ def generate_response(message):
 
 def main():
     st.set_page_config(
-        page_title="Assistant financial analyst for Telecom Egypt", page_icon=":book:")
+        page_title="AI to analyze economic data", page_icon=":book:")
 
-    st.header("Assistant financial analyst for Telecom Egypt :book:")
-    message = st.text_area("by Mohammed Elansary    v0.12")
+    st.header("AI to analyze economic data :book:")
+    message = st.text_area("by Mohammed Elansary    v0.14")
 
     if message:
         st.write("Generating your answer...")
 
         result = generate_response(message)
-        result = generate_response(message)
-        result = result.replace("+","}")
-        result = result.replace("_","{")
-        result = result.replace("@","%27")
+        #result = generate_response(message)
+        #result = result.replace("+","}")
+        #result = result.replace("_","{")
+        #result = result.replace("@","%27")
         #result = result.replace("[Chart Link]","![]")
 
         st.info(result)
